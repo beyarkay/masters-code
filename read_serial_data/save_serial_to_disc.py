847e05fd1b4dc676aaac30189a95cc83902bfc60
Kane, BR, Mr [26723077@sun.ac.za] <26723077@sun.ac.za>
2022-07-10 12:09:17 +0200

    Get real time prediction working

--- read_serial_data/save_serial_to_disc.py
+++ read_serial_data/save_serial_to_disc.py
@@ -1,15 +1,21 @@
 print("Importing libraries")
-from time import time
+from colors import color
+from matplotlib import cm
+from serial.tools.list_ports import comports
 from typing import Callable, List, Any
 import datetime
-from colors import color
-import os
 import numpy as np
-from matplotlib import cm
+from time import time
+import os
 import serial
-from serial.tools.list_ports import comports
 import sys
+import pickle
+script_dir = os.path.dirname( __file__ )
+utils_dir = os.path.join(script_dir, '..', 'machine_learning')
+sys.path.append(utils_dir)
+import common_utils as utils
 np.set_printoptions(threshold=sys.maxsize, linewidth=250)
+CLR = "\x1b[2K\r"
 
 
 def main():
@@ -20,7 +26,8 @@ def main():
     baudrate = 19_200
     print(f"Reading from {port} with baudrate {baudrate}")
     with serial.Serial(port=port, baudrate=baudrate, timeout=1) as ser:
-        loop_over_serial_stream(ser, save_to_disc)
+        # loop_over_serial_stream(ser, save_to_disc_cb)
+        loop_over_serial_stream(ser, predict_cb)
 
 
 def get_serial_port() -> str | None:
@@ -50,7 +57,11 @@ def loop_over_serial_stream(
     n_timesteps = 40
     n_sensors = 30
     first_loop = True
-    callback_data: dict[str, Any] = {
+
+    with open('../machine_learning/saved_models/idx_to_gesture.pickle', 'rb') as f:
+        idx_to_gesture = pickle.load(f)
+    # cb_data contains all data that get passed to the CallBack
+    cb_data: dict[str, Any] = {
         "n_timesteps": 40,
         "n_sensors": 30,
         "curr_offset": 0,
@@ -60,10 +71,17 @@ def loop_over_serial_stream(
         "n_measurements": 0,
         "gesture_idx": 0,
         "obs": np.full((n_timesteps, n_sensors), np.nan),
+        "time_ms": int(time() * 1000),
+        "prev_time_ms": int(time() * 1000),
+        "idx_to_gesture": idx_to_gesture,
     }
 
     while serial_handle.isOpen():
         try:
+            # record the current and previous times so we can calculate the
+            # duration between measurements
+            cb_data["prev_time_ms"] = cb_data["time_ms"]
+            cb_data["time_ms"] = int(time() * 1000)
             raw_values: List[str] = serial_handle.readline().decode('utf-8').strip().split(",")[:-1]
             # Ensure there are exactly 32 values
             if len(raw_values) == 0:
@@ -72,22 +90,22 @@ def loop_over_serial_stream(
             if len(raw_values) != n_sensors+2:
                 print(f"raw values are length {len(raw_values)}, not {n_sensors+2}: {raw_values}")
                 continue
-            callback_data["prev_gesture_idx"] = callback_data["gesture_idx"]
-            callback_data["gesture_idx"] = int(raw_values[0])
-            callback_data["prev_offset"] = callback_data["curr_offset"]
-            callback_data["curr_offset"] = int(raw_values[1])
-            callback_data["curr_idx"] = round(callback_data["curr_offset"] / 25)
+            cb_data["prev_gesture_idx"] = cb_data["gesture_idx"]
+            cb_data["gesture_idx"] = int(raw_values[0])
+            cb_data["prev_offset"] = cb_data["curr_offset"]
+            cb_data["curr_offset"] = int(raw_values[1])
+            cb_data["curr_idx"] = round(cb_data["curr_offset"] / 25)
             # If this is the first time looping through, then wait until the
             # beginning of a gesture comes around
             if first_loop:
-                if callback_data["curr_idx"] > 0:
+                if cb_data["curr_idx"] > 0:
                     continue
                 else:
                     first_loop = False
 
-            if callback_data["curr_idx"] == 40:
-                callback_data["curr_idx"] = 39
-            aligned_offset = callback_data["curr_idx"] * 25
+            if cb_data["curr_idx"] == 40:
+                cb_data["curr_idx"] = 39
+            aligned_offset = cb_data["curr_idx"] * 25
         except serial.serialutil.SerialException as e:
             print(f"Ergo has been disconnected: {e}")
             sys.exit(1)
@@ -104,10 +122,10 @@ def loop_over_serial_stream(
         ])
 
         # Call the callback
-        callback_data = callback(new_measurements, callback_data)
+        cb_data = callback(new_measurements, cb_data)
 
         now_str = datetime.datetime.now().isoformat()[11:-3]
-        curr_idx = callback_data["curr_idx"]
+        curr_idx = cb_data["curr_idx"]
         colors = []
         dims = ['x', 'y', 'z']
         for i, val in enumerate(raw_values[2:]):
@@ -125,7 +143,7 @@ def loop_over_serial_stream(
     else:
         print("Serial port closed")
 
-def save_to_disc(new_measurements: np.ndarray, d: dict[str, Any]) -> dict[str, Any]:
+def save_to_disc_cb(new_measurements: np.ndarray, d: dict[str, Any]) -> dict[str, Any]:
     """Given a series of new measurements, populate an observation and save to
     disc as require."""
 
@@ -161,12 +179,61 @@ def save_to_disc(new_measurements: np.ndarray, d: dict[str, Any]) -> dict[str, A
 def get_colored_string(value, n_values) -> str:
     colours = cm.get_cmap('turbo', n_values)
     rgb = [int(val * 255) for val in colours(value)[:-1]]
-    coloured = color(f'{str(value):3}', 'black', f'rgb({rgb[0]},{rgb[1]},{rgb[2]})')
+    mag = np.sqrt(sum([x * x for x in rgb]))
+    coloured = color(f'{str(value):3}', 'black' if mag > 180 else 'white', f'rgb({rgb[0]},{rgb[1]},{rgb[2]})')
     return coloured
+
+def predict_cb(new_measurements: np.ndarray, d: dict[str, Any]) -> dict[str, Any]:
+    """."""
+    
+    scaler = utils.load_model(
+        '../machine_learning/saved_models/StandardScaler().pickle'
+    )
+    clf = utils.load_model(
+        '../machine_learning/saved_models/MLPClassifier(alpha=8.97617892363434e-05,hidden_layer_sizes=50,max_iter=1000).pickle'
+    )
+    # Calculate how much time has passed between this measurement and the
+    # previous measurement, rounded to the nearest 25ms
+    diff = round((d["time_ms"] - d["prev_time_ms"]) / 25) * 25
+    if diff == 0:
+        # If no time has passed, just replace the most recent measurement with
+        # the new measurement
+        d["obs"][0, :] = new_measurements
+    elif diff == 25:
+        # If 25ms has passed, shuffle all measurements along and then insert
+        # the new measurement
+        d["obs"][1:, :] = d["obs"][:-1, :]
+        d["obs"][0, :] = new_measurements
+    elif diff > 25:
+        # If more than 25ms has passed, shuffle all measurements along by the
+        # number of 25ms increments
+        shift_by = diff // 25
+        d["obs"][shift_by:, :] = d["obs"][:-shift_by, :]
+        # Then add the new measurements to the first slot
+        d["obs"][0, :] = new_measurements
+        # And impute the missing values as the mean of the most recent
+        # measurements and the new measurements
+        avg = np.mean((d["obs"][0, :], d["obs"][shift_by, :]), axis=0)
+        for idx in range(1, shift_by):
+            d["obs"][idx, :] = avg
+
+    # If we have any NaNs, don't try predict anything
+    if np.isnan(d["obs"]).any():
+        print(f"{CLR}Not predicting, (obs contains {(np.isnan(d['obs'])).sum()} NaNs)")
+        return d
+
+    predictions = utils.predict_nicely(d["obs"], clf, scaler, d["idx_to_gesture"])
+    print(f"{CLR}", end='')
+    for gesture, proba in sorted(predictions, key=lambda gp: gp[0]):
+        colored = get_colored_string(int(proba*100), 100)
+        gesture = gesture.replace('gesture0', 'g')
+        print(f'{gesture:>6}:{colored:>2}%', end=',')
+    print()
+    return d
 
 if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        print("\x1b[2K\rFinishing")
+        print(f"{CLR}Finishing")
         sys.exit(0)
