783614635d9eebd20f74cc22298563d3836800b1
Kane, BR, Mr [26723077@sun.ac.za] <26723077@sun.ac.za>
2023-03-24 17:10:45 +0200

    Add HigherOrderFunction for i2g & g2i

--- src/common.py
+++ src/common.py
@@ -1,3 +1,4 @@
+"""A collection of commonly used functions/classes for Ergo."""
 from enum import Enum
 import typing
 import numpy as np
@@ -36,6 +37,35 @@ def check_X_y(X: np.ndarray, y: np.ndarray):
     assert type(y) is np.ndarray
     assert X.shape[0] == y.shape[0]
     return X, y
+
+
+def make_gestures_and_indices(y_str):
+    """Create two vectorized functions to convert gesture names to integer
+    indices and vice versa.
+
+    Includes a special case for the `gesture0255` gesture, since mapping
+    `gesture0255` => `255` causes all sorts of problems in tensorflow.
+
+    Args:
+    - y_str: A 1-dimensional ndarray containing the gesture names.
+
+    Returns:
+    A tuple containing two vectorized functions:
+    - to_index: A function that takes a gesture name and returns an integer index.
+    - to_gesture: A function that takes an integer index and returns a gesture name.
+    """
+
+    # Determine the maximum integer value, which is one less than the number of
+    # unique gestures.
+    maximum = len(np.unique(y_str)) - 1
+
+    # Define two vectorized functions to convert gestures to indices and vice
+    # versa.
+    to_index = np.vectorize(lambda g: int(g[-4:]) if g != "gesture0255" else maximum)
+    to_gesture = np.vectorize(
+        lambda i: f"gesture{i:0>4}" if i != maximum else "gesture0255"
+    )
+    return (to_index, to_gesture)
 
 
 class ControlFlow(Enum):
