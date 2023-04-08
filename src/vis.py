e5cfa3d4c801cd2d157e3ce5d533ef1420d5a73f
Kane, BR, Mr [26723077@sun.ac.za] <26723077@sun.ac.za>
2023-04-08 09:59:16 +0200

    Make plot_conf_mat function

--- src/vis.py
+++ src/vis.py
@@ -4,6 +4,9 @@ This should include functions for visualising results via a live GUI, as well
 as for visualising results via the CLI.
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 import logging as l
 import common
 import pred
@@ -26,3 +29,25 @@ class StdOutHandler(common.AbstractHandler):
         )
         l.info(f"Prediction: {str(pred_handler.prediction): <20} Truth: {truth: <20}")
         print(f"Prediction: {str(pred_handler.prediction): <20} Truth: {truth: <20}")
+
+
+def plot_conf_mats(model, Xs, ys, titles=None):
+    """Plots the confusion matrices for the given model with the given data.
+
+    The confusion matrices are log10 transformed to highlight low-occuring
+    misclassifications."""
+    conf_mats = []
+    for X, y in zip(Xs, ys):
+        conf_mats.append(model.confusion_matrix(y, X_to_pred=X).numpy())
+    vmax = np.log10(np.max(np.array(conf_mats).flatten()))
+    fig, axs = plt.subplots(1, len(conf_mats), figsize=(5 * len(conf_mats), 4))
+
+    for cm, ax, title in zip(conf_mats, axs, titles):
+        sns.heatmap(np.log10(cm), ax=ax, vmin=0, vmax=vmax, square=True)
+        ax.set(
+            xlabel="Predicted Gesture",
+            ylabel="Actual Gesture",
+        )
+        if titles is not None:
+            ax.set_title(title)
+    return fig, axs
