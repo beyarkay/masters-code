Fix classification report calculation error
Previously, the calculation of precision, recall, and F1 score could all
result in a division by zero error if a class existed but no predictions
were made for that class. That commonly happened in the case where a
model only ever predicted the majority gesture.

The sklearn method `classification_report` takes in an optional
parameter `zero_division` which was previously set to `np.nan`. This
meant that any zero division errors for a class would result in a
precision/recall/f1-score for that class of `np.nan`. However, when
calculating the averages over all classes, `np.nan` values are omitted,
so if there are 50 `nan` values and one `0.99` value, the "average" will
be 0.99. Not great.

This fix sets the `zero_division` parameter to zero, reflecting that if
a model never predicts a class, then it fails in that class.

src/models.py
@@ -316,7 +316,7 @@ class TemplateClassifier(BaseEstimator, ClassifierMixin):
                 y_pred.astype(int),
                 y.astype(int),
                 output_dict=True,
-                zero_division=np.nan,  # type: ignore
+                zero_division=0,  # type: ignore
             )
             clf_report = pd.json_normalize(report)  # type: ignore
 
@@ -429,7 +429,7 @@ class TemplateClassifier(BaseEstimator, ClassifierMixin):
             self.y_,
             y_trn_pred,
             output_dict=True,
-            zero_division=np.nan,
+            zero_division=0,
         ))
         clf_report_trn.columns = [f'trn.{c}' for c in clf_report_trn.columns]
 
@@ -476,7 +476,7 @@ class TemplateClassifier(BaseEstimator, ClassifierMixin):
                 y_pred,
                 y,
                 output_dict=True,
-                zero_division=np.nan,
+                zero_division=0,
             ))
             # Rename the columns to start with the correct prefix
             clf_report.columns = [f'{prefix}.{c}' for c in clf_report.columns]